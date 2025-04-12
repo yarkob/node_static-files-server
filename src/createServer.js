a79a1f7ef86d4c3ef89e34205494b5c0d194df56
yarkob <yaroslav.klark.bondarenko@gmail.com>
2025-04-12 10:17:01 +0300

    complete task

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,39 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const keyToFiles = '/file/';
+    const fileName = req.url.slice(keyToFiles.length);
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    if (req.url.startsWith('/file') && !req.url.includes(keyToFiles)) {
+      res.end('URL should start with /file/ to access a file');
+    }
+
+    if (req.url.includes(keyToFiles)) {
+      fs.readFile(path.resolve('public/', fileName), 'utf8', (err, data) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end('No such file');
+        }
+
+        if (fileName.includes('..')) {
+          res.statusCode = 400;
+          res.end("Can't access this file");
+        }
+
+        res.end(data);
+      });
+    } else {
+      res.statusCode = 400;
+      res.end();
+    }
+  });
 }
 
 module.exports = {
